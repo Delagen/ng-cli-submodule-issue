239cd204f7e3737a3ac3db0a72834cc38698da4e
Vohmyanin Sergey Vasilevich <delagen@gmail.com>
2018-01-13 15:00:30 +0300

    initial

---
+++ app-sub.component.ts
@@ -0,0 +1,13 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'app-sub',
+    templateUrl: './app-sub.component.html'
+})
+export class AppSubComponent implements OnInit {
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+}
---
+++ submodule.module.ts
@@ -0,0 +1,12 @@
+import {NgModule} from '@angular/core';
+
+import {AppSubComponent} from './app-sub.component';
+
+@NgModule({
+    imports: [],
+    exports: [AppSubComponent],
+    declarations: [AppSubComponent],
+    providers: [],
+})
+export class AppSubModule {
+}
